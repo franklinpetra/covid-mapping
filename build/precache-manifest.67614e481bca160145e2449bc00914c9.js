7b8c22baebd48dee14d2eb427159d3802cf5e507
franklin,petra <petrafranklin.gmail.com>
2020-09-09 15:25:06 -0700

    netlify deploy prep

---
+++ build/precache-manifest.67614e481bca160145e2449bc00914c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c06d43e80a4498ab6e5da7348d7fd8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdbbb1b8fed3096d3d3e",
+    "url": "/static/css/main.8a535638.chunk.css"
+  },
+  {
+    "revision": "e30a585d0088b2675464",
+    "url": "/static/js/2.d02a56bb.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.d02a56bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbbb1b8fed3096d3d3e",
+    "url": "/static/js/main.d0d48ccc.chunk.js"
+  },
+  {
+    "revision": "0f141f1a15ea4bbbb834",
+    "url": "/static/js/runtime-main.ca98175e.js"
+  },
+  {
+    "revision": "25ddb02cd8d184e73bb5a1f8ff97511e",
+    "url": "/static/media/Icon.25ddb02c.png"
+  },
+  {
+    "revision": "c66d5aa416ec0a3be30db74504e5e0fe",
+    "url": "/static/media/LineWearingMasks.c66d5aa4.jpeg"
+  },
+  {
+    "revision": "b5c4c2f34ff07cbbcd9f6fb39d4b9484",
+    "url": "/static/media/girlMasked.b5c4c2f3.jpeg"
+  }
+]);
